8f0953a137c002d2105efce933c3a4d3e502ab4f
vineethguna <vineeth.guna@gmail.com>
2016-05-09 17:06:36 +0530

    Fixed 404 issues on express front

--- executor/app.js
+++ executor/app.js
@@ -21,9 +21,10 @@ initializeRoutes(app, 'v1', specs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  // next(err);
+  res.send(404).end();
 });
 
 // error handlers
@@ -32,22 +33,22 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.status(err.status || 500).end();
+    // res.render('error', {
+    //   message: err.message,
+    //   error: err
+    // });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.status(err.status || 500).end();
+  // res.render('error', {
+  //   message: err.message,
+  //   error: {}
+  // });
 });
 
 
